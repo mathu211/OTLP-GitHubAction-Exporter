041e2eb92e842da6400f9529cb755aa5e759cd33
mathu211 <mathumitha211@gmail.com>
2025-06-11 14:32:42 +0530

    type print

--- src/exporter.py
+++ src/exporter.py
@@ -153,7 +153,7 @@ pcontext = trace.set_span_in_context(p_parent)
 print(job_lst)
 for job in job_lst:
     try:
-        print("Processing job ->",job['name'])
+        print("01: Processing job ->", '\n',job['name'], '\n', type(job), '\n', type(job['name']))
         if isinstance(job, str):
             job = json.loads(job)
         result = parse_attributes(job, "steps", "job")
