3923db4bd0021f5dbec400a4a2bd22cbd7d69d10
mathu211 <mathumitha211@gmail.com>
2025-06-11 17:10:22 +0530

    tbs6

--- src/custom_parser/__init__.py
+++ src/custom_parser/__init__.py
@@ -66,7 +66,6 @@ def parse_attributes(obj,att_to_drop,otype):
                                 for att in obj[attribute][sub_att]:
                                     attribute_name = do_string(attribute)+"."+do_string(sub_att)+"."+do_string(att)
                                     if attribute_name not in attributes_to_drop:
-                                        print("Check string")
                                         obj_atts[attribute_name]=str(obj[attribute][sub_att][att])
                                         if attribute_name.endswith("_at"):
                                             if obj['conclusion'] == 'skipped' or obj['conclusion'] == 'cancelled':
@@ -85,6 +84,7 @@ def parse_attributes(obj,att_to_drop,otype):
                                                 if attribute_name not in attributes_to_drop:
                                                     obj_atts[attribute_name]=str(key[att])
                                                     if attribute_name.endswith("_at"):
+                                                        print("Check string")
                                                         if obj['conclusion'] == 'skipped' or obj['conclusion'] == 'cancelled':
                                                             pass
                                                         else:
