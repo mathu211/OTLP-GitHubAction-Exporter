cbbe5f10db3b5ed7463f8afe4771144a7e74dd8f
mathu211 <mathumitha211@gmail.com>
2025-06-12 14:16:39 +0530

    check 02

--- src/custom_parser/__init__.py
+++ src/custom_parser/__init__.py
@@ -49,7 +49,6 @@ def parse_attributes(obj,att_to_drop,otype):
     #         print("Unable to parse GITHUB_ATTRIBUTES_DROP, check your configuration")
     for attribute in list(obj):
         attribute_name = str(attribute).lower()
-        # print("Check string: ", attribute_name)
         if attribute_name.endswith("_at"):
             if obj['conclusion'] == 'skipped' or obj['conclusion'] == 'cancelled':
                 pass
@@ -68,7 +67,7 @@ def parse_attributes(obj,att_to_drop,otype):
                                     attribute_name = do_string(attribute)+"."+do_string(sub_att)+"."+do_string(att)                                    
                                     if attribute_name not in attributes_to_drop:                                         
                                         obj_atts[attribute_name]=str(obj[attribute][sub_att][att])  
-                                        # print("Check string: ", attribute_name)                                     
+                                        print("Check string: ", attribute_name)                                     
                                         if attribute_name.endswith("_at"):        
                                             print("confirmed")                                     
                                             if obj['conclusion'] == 'skipped' or obj['conclusion'] == 'cancelled':                                                    
@@ -77,8 +76,9 @@ def parse_attributes(obj,att_to_drop,otype):
                                                 new_Att_name=attribute_name+"_ms"
                                                 obj_atts[new_Att_name]=do_time_ms(obj[attribute][sub_att][att])
 
-                                
+                            
                             elif type(obj[attribute][sub_att]) is list:
+                                print("List found in attribute: ", attribute)
                                 for key in obj[attribute][sub_att]:
                                     if type(key) is dict:
                                         for att in key:
@@ -139,5 +139,6 @@ def parse_attributes(obj,att_to_drop,otype):
                                     pass
                                 else:
                                     new_Att_name=attribute_name+"_ms"
-                                    obj_atts[new_Att_name]=do_time_ms(obj[attribute])           
+                                    obj_atts[new_Att_name]=do_time_ms(obj[attribute])
+                
     return obj_atts
